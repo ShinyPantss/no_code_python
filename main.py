a3105ab505e733542e0e49860f3a8b9fef86b77e
ShinyPantss <mucahitcan39@hotmail.com>
2024-03-19 21:29:23 +0300

    Cors update

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
-import os
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
