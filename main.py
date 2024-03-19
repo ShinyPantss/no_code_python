d29d6916da2d1493a0672f3d49ba7c0710f6799d
ShinyPantss <mucahitcan39@hotmail.com>
2024-03-19 23:02:22 +0300

    Plotting Sample

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
+import plot2d
+
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return jsonify({"Emreye Selam": "Hayirli olsun kardes"})
+    if request.method == "GET":
+        data = request.json
+        print(data)
+        plot2d.Plot2D(x=[1, 2, 3], y=[4, 5, 6]).show()
+    elif request.method == "POST":
+        return jsonify({"message": "Hello World"})
 
 
 if __name__ == "__main__":
---
+++ plot2d.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import io
+from PIL import Image
+
+
+class Plot2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def plot(self):
+        # plot
+        plt.plot(self.x, self.y)
+
+    def show(self):
+        # Getting the current figure
+        fig = plt.gcf()
+
+        # Convert the figure to a PIL Image
+        buf = io.BytesIO()
+        fig.savefig(buf)
+        buf.seek(0)
+        img = Image.open(buf)
+
+        # Show the image
+        img.show()
+
+
+# Creating list of Month and Share_buy for Plotting Line Graph
+Month = ["January", "February", "March"]
+Share_buy = [10, 17, 30]
+
+# Create an instance of Plot2D class
+plotter = Plot2D(Month, Share_buy)
+
+# Plot the data
+plotter.plot()
+
+# Show the plot
+plotter.show()
