b71ca4f0c488a60448868122dba481dfe3d38aa3
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-03-27 16:26:02 +0300

    fixed some sync

--- plot2d.py
+++ plot2d.py
@@ -1,70 +1,49 @@
 import matplotlib.pyplot as plt
-import io
-from PIL import Image
+import matplotlib
+import numpy as np
 
 
-class Plot2D:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def plot(self):
-        # plot
-        plt.plot(self.x, self.y)
-
-    def show(self):
-        # Getting the current figure
-        fig = plt.gcf()
-
-        # Convert the figure to a PIL Image
-        buf = io.BytesIO()
-        fig.savefig(buf)
-        buf.seek(0)
-        img = Image.open(buf)
-
-        # Show the image
-        img.show()
+from supabase import create_client
+import os
 
 
-# Creating list of Month and Share_buy for Plotting Line Graph
-Month = ["January", "February", "March"]
-Share_buy = [10, 17, 30]
+url: str = os.environ.get("SUPABASE_URL")
+print(url)
+key: str = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
 
-# Create an instance of Plot2D class
-plotter = Plot2D(Month, Share_buy)
 
-# Plot the data
-plotter.plot()
+class ImageUploader:
+    def __init__(self):
+        pass
 
-# Show the plot
-plotter.show()
-def create_and_upload_image(self):
-    matplotlib.use("Agg")
-    # Görüntü oluşturma
-    plt.ion()
-    x = np.linspace(0, 10, 100)
-    y = np.cos(x)
-    plt.plot(x, y)
-    plt.xlabel("x")
-    plt.ylabel("sin(x)")
-    plt.grid()
-    plt.title("emrenin grafiği")
+    def create_and_upload_image(self):
+        matplotlib.use("Agg")
+        # Görüntü oluşturma
+        plt.ion()
+        x = np.linspace(0, 10, 100)
+        y = np.cos(x)
+        plt.plot(x, y)
+        plt.xlabel("x")
+        plt.ylabel("sin(x)")
+        plt.grid()
+        plt.title("emrenin grafiği")
 
-    nameOFfile = "deneme_emre3.jpeg"
+        nameOFfile = "deneme_emre3.jpeg"
 
-    # Görüntüyü bir bayt akışına dönüştürme
-    x = plt.savefig(nameOFfile, format="jpeg")
-    with open(nameOFfile, "rb") as f:
-        byte_stream = f.read()
+        # Görüntüyü bir bayt akışına dönüştürme
+        x = plt.savefig(nameOFfile, format="jpeg")
+        with open(nameOFfile, "rb") as f:
+            byte_stream = f.read()
 
-        # Bayt akışını yükle
-        supabase.storage.from_("deneme").upload(
-            nameOFfile,
-            byte_stream,
-            file_options={"content-type": "image/jpeg"},
+            # Bayt akışını yükle
+            supabase.storage.from_("deneme").upload(
+                nameOFfile,
+                byte_stream,
+                file_options={"content-type": "image/jpeg"},
+            )
+        link_of_image = supabase.storage.from_("deneme").get_public_url(
+            nameOFfile
         )
-    link_of_image = supabase.storage.from_("deneme").get_public_url(
-        nameOFfile
-    )
-    return link_of_image
+        return link_of_image
 
