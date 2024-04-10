868034864d7ec6dc18bd48021d2cd257a8a1825d
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-04-10 23:56:19 +0300

    update main.py

--- main.py
+++ main.py
@@ -22,7 +22,21 @@ def index():
         # return url
         return "GET request recieved"
     elif request.method == "POST":
+<<<<<<< Updated upstream
         data = request.get_json()
+=======
+        data = request.json
+        # title = data.get('title', 'unnamed')  # .get(,*) there exists second parameter to provide a default value, 
+        # if get func does not return a value
+        # x = plot2d.ImageUploader()
+        # print("name----->",x.nameOFfile)
+        # url = x.create_and_upload_image()
+        # plot = simplePlot.SimplePlot(y_column=[randint(1,10),randint(1,10),randint(1,10),randint(1,10)])
+        plot = scatterPlot.ScatterPlot(y_column=[randint(1,10),randint(1,10),randint(1,10),randint(1,10)],x_column=[randint(1,10),randint(1,10),randint(1,10),randint(1,10)],grid=True,title="title")
+        url = plot.scatterPlot()
+        print("url---->",url)
+        return url
+>>>>>>> Stashed changes
         
         print(f"\n\n\n\n\n{data}\n\n\n\n\n")
         plot = classifer.Classifer(data)
