56f7382fb09acff4d762c49ab22d2126136e9298
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-04-12 23:27:59 +0300

    yeni branch deneme
    yeni branch açmayı deniyorum

--- barPlot.py
+++ barPlot.py
@@ -4,27 +4,32 @@ import time
 import matplotlib
 
 class BarPlot:
-    def __init__(self, x_column=None, y_column=list(),
-                 title='unnamed', xLabel='x axis', yLabel='y axis',
-                 width=0.8, grid=False):
+    def __init__(self, data, y_column=[1, 2, 3]):
 
-        if x_column is None:
+        self.data = data["data"]
+        
+        # group 1 parameters - y axis parameters and x axis parameters
+        self.y_column = y_column
+        self.x_column = self.data.get("x_column", None)
+        
+        # group 2 parameters -  title, xLabel, yLabel
+        self.title = self.data.get("title", 'unnamed')
+        self.xLabel = self.data.get("xLabel", 'x axis')
+        self.yLabel = self.data.get("yLabel", 'y axis')
+        
+        # group 3 parameters - width, grid
+        self.width = [self.data.get("Width", 0.8)]*len(y_column)  # for each bar needs one width value
+        self.grid = self.data.get("grid", False)
+        # other parameters can be added
+        
+        if self.x_column is None:
             # if user does not prefer to enter x axis parameters,
             # x column will be arranged as the length of y axis parameters
-            x_column = range(len(y_column))
-
-        self.x_column = x_column
-        self.y_column = y_column
-        self.xLabel = xLabel
-        self.yLabel = yLabel
-        self.title = title
-        self.width = [width for each in range(len(y_column))]
-        # for each bar needs one width value
-        self.grid = grid
-        pass
+            self.x_column = range(len(y_column))
 
     def barPlot(self):
-        # plt.style.use('_mpl-gallery')
+        # summary: this function creates a bar plot graph and returns the image url
+        
         matplotlib.use('agg')  # necessary
         plt.ion()  # necessary
         
@@ -32,9 +37,11 @@ class BarPlot:
         
         ax.bar(self.x_column, self.y_column, width=self.width)
 
+        # group 1 parameters(from graph doc classification)
         plt.title(self.title)
         plt.xlabel(self.xLabel)
         plt.ylabel(self.yLabel)
+        
         plt.grid(self.grid)
         ax.set_axisbelow(True)  # this line provides to take grids to back of bars
         # ax.yaxis.grid(color='gray', linestyle='-', linewidth=0.5)
@@ -42,7 +49,7 @@ class BarPlot:
         
         graphName = str(time.time()).replace(".","")+".jpeg"
         plt.savefig(graphName, format="jpeg")
+        
         barGraph = plot2d.ImageUploader(graphName)
         return barGraph.create_and_upload_image()
-        # plt.show()
 
