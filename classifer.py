6abdcd8d622eeeb0d5987f4fef7c31fa83eb1257
ShinyPantss <mucahitcan39@hotmail.com>
2024-04-15 17:41:01 +0300

    fix main return url

--- classifer.py
+++ classifer.py
@@ -8,24 +8,27 @@ class Classifer:
 
         self.plotType = data.get("plotType", None)
         self.data = data
-        
+
     def classify(self):
-        
+
         if self.plotType == "simplePlot":
-            
+
             plot = simplePlot.SimplePlot(data=self.data)
             url = plot.simplePlot()
-            
+
         elif self.plotType == "barPlot":
-            
+
             plot = barPlot.BarPlot(data=self.data)
+
             url = plot.barPlot()
-            
+
         elif self.plotType == "scatterPlot":
             plot = scatterPlot.ScatterPlot(data=self.data)
+
             url = plot.scatterPlot()
-            
+
         else:
             url = "invalid plot type"
-            
+
+        
         return url
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import os
 from random import randint
 import classifer
+from flask import jsonify
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,18 +15,17 @@ def index():
 
     if request.method == "GET":
         return "GET request recieved"
-    
+
     elif request.method == "POST":
 
         data = request.get_json()
-        
-        print(f"\n\n\n\n\n{data}\n\n\n\n\n")
+
         plot = classifer.Classifer(data)
 
         url = plot.classify()
-        print(f"\n\n\n\n\n{url}\n\n\n\n\n")
-        
-        return url
+
+        return jsonify(url)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
