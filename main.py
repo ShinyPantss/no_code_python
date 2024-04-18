3cf3eb81e3c455bd5f94c05d663d744122e2646f
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-04-18 16:01:57 +0300

    main module bug fixed
    bugs fixed and data was cleaned

--- main.py
+++ main.py
@@ -19,10 +19,8 @@ def index():
     
     elif request.method == "POST":
         data = request.get_json()
-        print(data)
 
         plot = classifer.Classifer(data)
-
         url = plot.classify()
 
         return jsonify(url)
@@ -33,29 +31,10 @@ def api():
         return "GET request recieved"
     
     elif request.method == "POST":
-        
         data = request.get_json()
-        print(data)
-
-        plot = classifer.Classifer(data)
-
-        url = plot.classify()
-
-        return jsonify(url)
-
-@app.route("/api", methods=["GET", "POST"])
-def api():
-    if request.method == "GET":
-        return "GET request recieved"
-    
-    elif request.method == "POST":
         
-        data = request.get_json()
-
-        # dataPool = datasetpuller.DSpull(data["api"])
-        # above line creates an instance of the DSpull class with the api as the url
-        # columns = dataPool.get_data()
         columns= datasetpuller.DSpull(data["api"]).get_columns()
+        # above line creates an instance of the DSpull class with the api as the url then calls the get_columns method on it
         return jsonify(columns)
 
 if __name__ == "__main__":
