476e007d67f311113d9b9e6ad388e65f463070c5
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-04-18 15:39:55 +0300

    datasetpuller module update
    the module can get data on api, filter, choose columns and return them.
    -main module can send to web backend

--- datasetpuller.py
+++ datasetpuller.py
@@ -1,27 +1,31 @@
 import requests
 import json
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class DSpull:
-    def __init__(self, url):
-        self.url = url
-        self.response = requests.get(url)
-        self.data = json.loads(self.response.text)
-        
+    def __init__(self, api):
+        self.url = api
+        self.response = requests.get(api)
 
-    
 
-    def get_data(self):
+    def get_columns(self):
         if self.response.status_code == 200:
             # Parse the response as JSON
             self.data = json.loads(self.response.text)
+            df = pd.DataFrame(self.data["data"])
+            # self.data includes the keys "data" and "source" so we need to access the "data" key to get the actual data
+
+            columns = list(df.columns) # type of df.columns is pandas.core.indexes.base.Index, so we convert it to a list
             
-            # Extract the years and average wages
-            years = [int(item['Year']) for item in self.data['data']]
-            wages = [float(item['Average Wage']) for item in self.data['data']]
+            indexesOFID = [index for index in range(len(columns)) if "ID" in columns[index]]            
+            # indexesOFID includes the indexes of the columns that have "ID" in their name  
+                            
+            columns_filtered = [columns[i] for i in range(len(columns)) if i not in indexesOFID]
+            # above line removes the columns that have "ID" in their name from the list of columns
             
-            return self.data
-        
+            return columns_filtered
+
         else:
             error = f"Request failed with status code {self.response.status_code}"
             
--- main.py
+++ main.py
@@ -3,36 +3,60 @@ from flask_cors import CORS
 import os
 from random import randint
 import classifer
+from flask import jsonify
+import datasetpuller
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/plot", methods=["GET", "POST"])
 def index():
 
     if request.method == "GET":
-        # data = request.title
-        # print(data)
-        # x = plot2d.ImageUploader()
-        # print("name----->",x.nameOFfile)
-        # url = x.create_and_upload_image()
-        # plot = simplePlot.SimplePlot(y_column=[randint(1,10),randint(1,10),randint(1,10),randint(1,10)])
-        # url = plot.simplePlot()
-        # return url
+
         return "GET request recieved"
+    
     elif request.method == "POST":
         data = request.get_json()
-        classifer(data)
-        # plot = scatterPlot.ScatterPlot(
-        #     y_column=[randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10)],
-        #     x_column=[randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10)],
-        #     grid=True,
-        #     title=title,
-        # )
-        # url = plot.scatterPlot()
-        return "geldi"
+        print(data)
 
+        plot = classifer.Classifer(data)
+
+        url = plot.classify()
+
+        return jsonify(url)
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+    if request.method == "GET":
+        return "GET request recieved"
+    
+    elif request.method == "POST":
+        
+        data = request.get_json()
+        print(data)
+
+        plot = classifer.Classifer(data)
+
+        url = plot.classify()
+
+        return jsonify(url)
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+    if request.method == "GET":
+        return "GET request recieved"
+    
+    elif request.method == "POST":
+        
+        data = request.get_json()
+
+        # dataPool = datasetpuller.DSpull(data["api"])
+        # above line creates an instance of the DSpull class with the api as the url
+        # columns = dataPool.get_data()
+        columns= datasetpuller.DSpull(data["api"]).get_columns()
+        return jsonify(columns)
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
